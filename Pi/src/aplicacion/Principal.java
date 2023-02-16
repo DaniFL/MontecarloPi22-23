f43d0001706ff48ca1f578e1ccbf4b429077761a
Danii <danielfernandezlopez75@gmail.com>
2023-02-16 12:45:44 +0100

    javadoc falta diagrama

--- Pi/src/aplicacion/Principal.java
+++ Pi/src/aplicacion/Principal.java
@@ -1,30 +1,31 @@
-// Copyright [2023] [Daniel Fernández López]
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific
-// language governing permissions and limitations under theLicense.
+/* Copyright [2023] [Daniel Fernández López]
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ either express or implied. See the License for the specific
+ language governing permissions and limitations under theLicense.*/
 
 package aplicacion;
 
 import dominio.Matematicas;
 
 /**
- * The type Principal.
+ * Clase Principal que alberga el método main del programa.
  */
 public class Principal {
     /**
-     * The entry point of application.
+     * Método main del programa.
      *
-     * @param args the input arguments
+     * @param args argumentos que recibe el programa.
      */
     public static void main(String[] args) {
+        // Muestra por pantalla el valor de PI.
         System.out.println("El valor de PI es: " + Matematicas.calcularPi(1000000));
     }
 }
--- Pi/src/dominio/Matematicas.java
+++ Pi/src/dominio/Matematicas.java
@@ -1,39 +1,49 @@
-// Copyright [2023] [Daniel Fernández López]
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific
-// language governing permissions and limitations under theLicense.
+/* Copyright [2023] [Daniel Fernández López]
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ either express or implied. See the License for the specific
+ language governing permissions and limitations under theLicense.*/
 
 package dominio;
 
 import java.util.Random;
+
+/**
+ * Clase Matematicas en la que se desarrolla la aproximación.
+ */
 public class Matematicas {
 
+/**
+     * Calcular pi double.
+     *
+     * @param n the n
+     * @return the double
+     */
     public static double calcularPi(int n) {
 
-        int inside = 0; // número de puntos dentro del círculo
+        int aciertos = 0; // número de puntos dentro del círculo
 
-        // generar n puntos aleatorios
-        Random rand = new Random();
+        // generar (n) puntos aleatorios
+        Random rand = new Random(); // generador de números aleatorios
         for (int i = 0; i < n; i++) {
             double x = rand.nextDouble(); // coordenada x
             double y = rand.nextDouble(); // coordenada y
             if (x * x + y * y <= 1) { // punto dentro del círculo
-                inside++;
+                aciertos++;
             }
         }
 
         // calcular el valor aproximado de Pi
-        double circleArea = inside / (double) n;
-        double squareArea = 1;
-        double pi = (circleArea * 4) / squareArea;
+        double areaCirculo = aciertos / (double) n;
+        double areaCuadrado = 1;
+        double pi = (areaCirculo * 4) / areaCuadrado;
         return pi;
     }
 }
